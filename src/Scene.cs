5eea056526a020f9246d7fdba98ab526499d5349
Frostyy <juniorjegej@gmail.com>
2026-05-15 18:04:44 +0200

    tyhth

--- src/Scene.cs
+++ src/Scene.cs
@@ -69,7 +69,7 @@ public class Scene : IDisposable
         string treeTexDir = ResolveDir("src", "textures", "trees"); 
         var treeModels = CacheModels(gl, selectedTreeFiles, treeTexDir);
 
-        const int treeCount = 30;
+        const int treeCount = 38;
         const float treeRingMin = 28f;
         const float treeRingMax = 37f;
         for (int i = 0; i < treeCount; i++)
